a056f6c7e3068ba4b706594c3d4f587d52c06613
Nemanja Miljkovic <proof1337@gmail.com>
2019-09-26 22:06:07 +0200

    Build gh pages

---
+++ precache-manifest.e55a3ac35dda8f909d7f3c93ef070e14.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9643bd9077fc6d83a58fc2f06500fa79",
+    "url": "/guitaramp/index.html"
+  },
+  {
+    "revision": "c5572f3b853d3af503d1",
+    "url": "/guitaramp/static/css/2.f0a787a4.chunk.css"
+  },
+  {
+    "revision": "c10d71f4a2e7c88b8f7d",
+    "url": "/guitaramp/static/css/main.dd63e283.chunk.css"
+  },
+  {
+    "revision": "c5572f3b853d3af503d1",
+    "url": "/guitaramp/static/js/2.768aaf01.chunk.js"
+  },
+  {
+    "revision": "c10d71f4a2e7c88b8f7d",
+    "url": "/guitaramp/static/js/main.d4734994.chunk.js"
+  },
+  {
+    "revision": "9f8a78c4afc59469fb1e",
+    "url": "/guitaramp/static/js/runtime~main.8fd6ea80.js"
+  },
+  {
+    "revision": "62b177ebe16f4422f32c845ec8846cf5",
+    "url": "/guitaramp/static/media/Allure_59_Tweed_P10N.62b177eb.wav"
+  },
+  {
+    "revision": "9f93c5dfc3b268003dc798c6e8c80e2d",
+    "url": "/guitaramp/static/media/Allure_64_A30_G12.9f93c5df.wav"
+  },
+  {
+    "revision": "1f523840c75c4fffb4de0d21b16444e5",
+    "url": "/guitaramp/static/media/Allure_64_USDeluxe_P12N.1f523840.wav"
+  },
+  {
+    "revision": "49f49da7c1e897c5435fd4c64e19ed61",
+    "url": "/guitaramp/static/media/Allure_67_Brit_Greenback.49f49da7.wav"
+  },
+  {
+    "revision": "d2bdc216f0cb47fbd02bd07fce48b21c",
+    "url": "/guitaramp/static/media/Marshall1960.d2bdc216.wav"
+  },
+  {
+    "revision": "a96aabe58ea80f7e839993dca1c13a04",
+    "url": "/guitaramp/static/media/MesaBoogie57TS.a96aabe5.wav"
+  },
+  {
+    "revision": "0ecb4c8553e2113d81b34d070c6a8e37",
+    "url": "/guitaramp/static/media/Plexi Lead Channel.0ecb4c85.wav"
+  },
+  {
+    "revision": "50f38887bd02c0416d4e2c1bebe0c06f",
+    "url": "/guitaramp/static/media/knob.50f38887.png"
+  },
+  {
+    "revision": "2c0a952801b967988cf59a4b5746ab8b",
+    "url": "/guitaramp/static/media/parking-garage.2c0a9528.wav"
+  },
+  {
+    "revision": "ce654815cc575656a2c343807a92d814",
+    "url": "/guitaramp/static/media/pcm90cleanplate.ce654815.wav"
+  },
+  {
+    "revision": "5e8146d94ab199eb33bcf239c802ea55",
+    "url": "/guitaramp/static/media/scala-milan-opera.5e8146d9.wav"
+  },
+  {
+    "revision": "7fa61919070010fd20bd96b53e8eead1",
+    "url": "/guitaramp/static/media/vox_tweed_1x12.7fa61919.wav"
+  }
+]);
